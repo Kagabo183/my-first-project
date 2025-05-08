8d57aaf890b02a59b771d7fdf4551428d8da7dce
Kagabo183 <kagaboriziki@gmail.com>
2025-05-08 15:07:55 +0200

    First commit

---
+++ test.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+import pandas as pd
+
+# Example DataFrame
+df = pd.DataFrame({
+    'Name': ['Alice', 'Bob'],
+    'Age': [25, 30]
+})
+
+print(df)
+
+df1 = df.dropna()
+print(df1)
